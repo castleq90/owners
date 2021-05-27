742728e7d83b9216e4827708a64676f3db7df62a
김성규 <castleq37@gmail.com>
2021-05-27 19:23:29 +0900

    Owners 과제완료

--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Owners(models.Model):
+    name  = models.CharField(max_length=45)
+    email = models.EmailField(max_length=300)
+    age   = models.IntegerField()
+
+
+    class Meta:
+        db_table = 'owners'
+
+class Dogs(models.Model):
+    name   = models.CharField(max_length=45)
+    age    = models.IntegerField()
+    owners = models.ForeignKey(Owners, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'dogs'
